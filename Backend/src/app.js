d172c398dd20a763b381c32dd87bb2fedeced09b
piyush <piyushujgaokar544@gmail.com>
2026-03-22 02:37:05 +0530

    deploying

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import authRouter from './routes/auth.routes.js'
 import chatRouter from './routes/chat.routes.js'
 import morgan from 'morgan'
 import cors from 'cors'
-
+import path from 'path'
 
 
 const app=express()
@@ -19,7 +19,13 @@ app.use(cors({
     methods:['GET','POST','PUT','DELETE']
 }))
 
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.use('/api/auth',authRouter)
 app.use('/api/chats',chatRouter)
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 export default app
